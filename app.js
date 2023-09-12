9ff5b07b36ce0759af4267139b700c025bcf4a10
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-09-12 15:31:24 +0000

    mounting

--- app.js
+++ app.js
@@ -2,18 +2,12 @@ import express from "express";
 const app = express();
 
 const port = process.env.PORT || 8080;
+//Import Routers
+import userRouter from "./routes/userRouter.js";
+import adminRouter from "./routes/adminRouter.js";
 
-app.get("/",(req,res)=>{
-    res.send("<h1> I am a Day 0 to ExpressJS. I am committed to stay here for long time...");
-});
-
-app.get("/junaid",(req,res)=>{
-    res.send("<h1> Dont forget to deploy your mvc app");
-});
-
-app.post("/junaid",(req, res)=>{
-    res.send("<h1> Dont forget to order a pizza to everyone");
-});
+app.user("/users", userRouter);//This /users is mounted to userRouter and userRouter has its routers in its file
+app.user("/admin", adminRouter);//This /admin is mounted to adminRouter and adminRouter has its routers in its file
 
 app.listen(port, () =>{
     console.log(`Server has Started at ${port}`);
--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,15 +3,15 @@ import express from "express";
 const router = express.Router();
 
 router.get("/home", (req,res)=>{
-    res.send("<h1> This Users is /home</h1>");
+    res.send("<h1> This is Users is /home</h1>");
 });
 
 router.get("/about", (req,res)=>{
-    res.send("<h1> This Users is /about </h1>");
+    res.send("<h1> This is Users is /about </h1>");
 });
 
 router.get("/contact", (req,res)=>{
-    res.send("<h1> This Users is /contact</h1>");
+    res.send("<h1> This is Users is /contact</h1>");
 });
 
 export default router;
