902f1333e986a6c59d0c88135de71049d40fff7a
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-09-12 15:55:11 +0000

    mounting

--- app.js
+++ app.js
@@ -6,8 +6,12 @@ const port = process.env.PORT || 8080;
 import userRouter from "./routes/userRouter.js";
 import adminRouter from "./routes/adminRouter.js";
 
-app.user("/users", userRouter);//This /users is mounted to userRouter and userRouter has its routers in its file
-app.user("/admin", adminRouter);//This /admin is mounted to adminRouter and adminRouter has its routers in its file
+app.get("/", (req, res)=>{
+    res.send("We are on limited TIME. Time is leaking....");
+});
+
+app.use("/users", userRouter);//This /users is mounted to userRouter and userRouter has its routers in its file
+app.use("/admin", adminRouter);//This /admin is mounted to adminRouter and adminRouter has its routers in its file
 
 app.listen(port, () =>{
     console.log(`Server has Started at ${port}`);
