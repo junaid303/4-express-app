fc5c53cf30bd60ad84bb2e9b37fd146cd60a243b
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-09-12 12:46:26 +0000

    User agent configuration

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -10,5 +10,8 @@ router.get("/about", (req,res)=>{
     res.send("<h1> This Users is /about");
 });
 
+router.get("/contact", (req,res)=>{
+    res.send("<h1> This Users is /contact");
+});
 
 export default router;
