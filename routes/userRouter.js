2bb72c9419a83d5005e3ff2799cddc5d52ddfa5a
junaid303 <mohammedjunaidkhan7@gmail.com>
2023-09-12 12:47:45 +0000

    User agent configuration

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,15 +3,15 @@ import express from "express";
 const router = express.Router();
 
 router.get("/home", (req,res)=>{
-    res.send("<h1> This Users is /home");
+    res.send("<h1> This Users is /home</h1>");
 });
 
 router.get("/about", (req,res)=>{
-    res.send("<h1> This Users is /about");
+    res.send("<h1> This Users is /about </h1>");
 });
 
 router.get("/contact", (req,res)=>{
-    res.send("<h1> This Users is /contact");
+    res.send("<h1> This Users is /contact</h1>");
 });
 
 export default router;
